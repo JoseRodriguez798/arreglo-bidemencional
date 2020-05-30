3d472407d7a4b605b460e42a0ece23ecb914c2e4
JoseRodriguez798 <64663529+JoseRodriguez798@users.noreply.github.com>
2020-05-29 20:44:25 -0600

    first commit

---
+++ src/arreglo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+static int getLength(string arreglo)
+{
+    return arreglo.length();
+}
+
+int main()
+{
+    string data;
+    //10
+    //  data.append("Jose Alber");
+
+    //50
+    //data.append("En la actualidad, la noción de programación se e");
+
+    //100
+    //data.append("En la actualidad, la noción de programación se encuentra muy asociada a la creación de aplicacion");
+
+    //500
+    //data.append("Programación es la acción y efecto de programar. Este verbo tiene varios usos: se refiere a idear y ordenar las acciones que se realizarán en el marco de un proyecto; al anuncio de las partes que componen un acto o espectáculo; a la preparación de máquinas para que cumplan con una cierta tarea en un momento determinado; a la elaboración de programas para la resolución de problemas mediante ordenadores y a la preparación de los datos necesarios para obtener una solución de un problema s");
+     
+
+    //2500
+    data.append("Programación es la acción y efecto de programar requiere del trabajo de diferentes especialistas. En principio, partiendo de la base de un proyecto bien organizado, es necesario dar con una idea atractiva, interesante, que justifique los meses o años de esfuerzo que vendrán. Tan sólo esta primera parte puede tomar mucho tiempo, dado que lo que comienza como un producto perfecto puede convertirse, luego de cuestionarlo y observarlo desde diferentes ángulos, en un fracaso seguro.En la actualidad, la noción de programación se encuentra muy asociada a la creación de aplicaciones informáticas y videojuegos; es el proceso por el cual una persona desarrolla un programa valiéndose de una herramienta que le permita escribir el código (el cual puede estar en uno o varios lenguajes, tales como C++, Java y Python) y de otra que sea capaz de «traducirlo» a lo que se conoce como lenguaje de máquina, el cual puede ser entendido por un microprocesador.Este último paso se conoce como compilación y es necesario para que el código pueda ser ejecutado por la plataforma para la cual haya sido creado, que puede ser un ordenador, una tableta, una consola o un teléfono móvil, por ejemplo. Existe también una forma de traducir el código denominada interpretación, que consiste en analizar línea a línea, hasta que se traduzca lo suficiente como para poder realizar una tarea. Cabe mencionar que los lenguajes de programación se dividen en dos grandes grupos, donde los que pueden ser compilados no pueden ser interpretados, y viceversa. ahsdkaduahosdhasdoiasd ashd ad ajdiaj dja odhas djaoidja dpia jdpajd aiydo ejkh fhai jfja houfpia jdpa da pr eif aohasj fparheojfapu au pajda dpa yre rper aid adhaoi fajp fiahf ajfash ouahf ahfo afiyeohgse fhai aif ahsduahs haou oageuya ohfuahf ojar ioua ouaohf oasiuproaiupa jfoiahfpia jpfap fpaj fpasfpo afpapof sasf pafajf pajfp ajfpoasjf pjaspfj apfj afjapfj piaj pasjfp ajfpj j apjfp ajf pasjfj pi  pjafjasj iaj pjfa pjfap fpajfpiap jaspisjfa pjfpa dlkwnlan la  jasdj wpd i qdpaj kajspjp ajepfajs ifa jkpaskjdieuf fajdiau aja jp ahsuha shiasji asj iaisjd isdoau uayu afaof oauf auifua fuoauf oiaufaoi fuiasu uasof aiofoaiu oiasu foiafa ofahfohfkjafkdhsfksfhfsk d  jk hk fkahfj ahkfha fhafas oifuaoi ffoia ufoiaofhaohfoero brhqohrjqwrqwr oqhoqo sdqowr jdoao fjiqef oe rqhruq rroiq roie asohsao ashoifgoah ohgoahgoid h ogha hfiuah lgskfh ajfhao hue oanfafahofh oashf oah o oa flk asid aihdaso hdohas dhaso dhoas dhhsada osal agjpd ogp nlgha");
+    cout << getLength(data) << endl;
+    return 0;
+}
